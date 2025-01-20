97d2c4f999a745d4b081f0a61fd4e4d4d672c971
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-01-20 20:39:34 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (64.09%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -2,24 +2,36 @@ class Solution {
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n=edges.size();
-        vector<vector<int>>adj(n+1);
+        vector<int> par(n+1), rank(n+1,1);
+        for(int i=0;i<=n;i++){
+            par[i]=i;
+        }
         for(const auto& edge:edges){
-            int u=edge[0];
-            int v=edge[1];
-            adj[u].push_back(v); 
-            adj[v].push_back(u); 
-            vector<bool> visit(n+1,false);
-            if(dfs(u,-1,adj,visit)) return {u,v};
+            if(!Union(par,rank,edge[0],edge[1])) return vector<int>{edge[0],edge[1]};
         }
     return {};
     }
-    bool dfs(int node,int parent,vector<vector<int>>& adj,vector<bool>& visit){
-        if(visit[node]) return true;
-        visit[node]=true;
-        for(const auto& nei:adj[node]){
-            if(nei==parent)continue;
-            if(dfs(nei,node,adj,visit)) return true;
+    int Find(vector<int>&par,int n){
+        int p=par[n];
+        while(p!=par[p]){
+            par[p] = par[par[p]];
+            p = par[p];
         }
-    return false;
+    return p;
+    }
+    bool Union(vector<int>& par,vector<int>& rank,int n1,int n2){
+        int p1=Find(par,n1);
+        int p2=Find(par,n2);
+
+        if(p1==p2)
+            return false;
+        else if(rank[p1]>rank[p2]){
+            par[p2]=p1;
+            rank[p1]+=rank[p2];
+        } else {
+            par[p1]=p2;
+            rank[p2]+=rank[p1];
+        }
+    return true; 
     }
 };
