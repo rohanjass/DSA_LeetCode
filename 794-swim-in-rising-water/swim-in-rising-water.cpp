8a39df30cd8eab9c4b669dd851a782d4d74a6e79
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-02-10 20:59:04 +0530

    Time: 28 ms (15.49%) | Memory: 17.1 MB (12.93%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
         int n=grid.size();
-        set<pair<int,int>> visit;
+        set<pair<int,int>>visited;
         priority_queue<vector<int>,vector<vector<int>>,greater<>>minH;
-        vector<vector<int>> dir={{0,1},{0,-1},{1,0},{-1,0}};
+        vector<vector<int>>dir={{1,0},{0,1},{-1,0},{0,-1}};
         minH.push({grid[0][0],0,0});
-        visit.insert({0,0});
+        visited.insert({0,0});
         while(!minH.empty()){
             auto curr=minH.top();
             minH.pop();
             int t=curr[0],r=curr[1],c=curr[2];
             if(r==n-1 && c==n-1) return t;
             for(const auto& d:dir){
-                int row=r+d[0],col=c+d[1];
-                if(row<0 || col<0 || row==n || col==n || visit.count({row,col})) continue;
-                visit.insert({row,col});
+                int row=r-d[0];
+                int col=c-d[1];
+                if(row<0 || col<0 || row==n || col==n ||visited.count({row,col})) continue;
+                visited.insert({row,col});
                 minH.push({max(t,grid[row][col]),row,col});
             }
         }
