33b92b488d554f60c95377717034996ae0f8c474
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-02-12 17:31:42 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (30.04%) - LeetSync

---
+++ 97-interleaving-string/interleaving-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    vector<vector<int>>dp;
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int m=s1.size(),n=s2.size();
+        if(m+n!=s3.size()) return false;
+        dp=vector<vector<int>>(m+1,vector<int>(n+1,-1));
+    return dfs(0,0,0,s1,s2,s3);
+    }
+    bool dfs(int i,int j,int k,string& s1, string& s2, string& s3){
+        if(k==s3.size()) return (i==s1.size()) && (j==s2.size());
+        if(dp[i][j]!=-1) return dp[i][j];
+        bool res=false;
+        if(i<s1.size() && s1[i]==s3[k]){
+            res=dfs(i+1,j,k+1,s1,s2,s3);
+        }
+        if(!res && j<s2.size() && s2[j]==s3[k]){
+            res=dfs(i,j+1,k+1,s1,s2,s3);
+        }
+        dp[i][j]=res;
+    return res;
+    }
+};
