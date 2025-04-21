8b194ef9e8f0b14451ea461282489375623f9d12
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-04-21 12:23:16 +0530

    Time: 903 ms (14.24%) | Memory: 11.5 MB (47.68%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    bool check(int ind,int i,int j,vector<vector<char>>& board, string word){
+    bool dfs(int ind,int i,int j,vector<vector<char>>& board,string word){
         if(ind==word.size()) return true;
         if(i<0 || j<0 || i==board.size() || j==board[0].size() || board[i][j]!=word[ind] || board[i][j]=='!') return false;
         char c=board[i][j];
         board[i][j]='!';
-        bool left=check(ind+1,i,j-1,board,word);
-        bool right=check(ind+1,i,j+1,board,word);
-        bool upper=check(ind+1,i-1,j,board,word);
-        bool lower=check(ind+1,i+1,j,board,word);
+        bool left=dfs(ind+1,i,j-1,board,word);
+        bool right=dfs(ind+1,i,j+1,board,word);
+        bool upper=dfs(ind+1,i-1,j,board,word);
+        bool bottom=dfs(ind+1,i+1,j,board,word);
         board[i][j]=c;
-        return left || right || upper || lower;
+    return left||right||upper||bottom;
     }
     bool exist(vector<vector<char>>& board, string word) {
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
-                if(check(0,i,j,board,word)) return true;
+                if(dfs(0,i,j,board,word)) return true;
             }
         }
     return false;
