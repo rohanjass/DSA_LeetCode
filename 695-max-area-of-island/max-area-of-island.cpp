b6e292dcf4294f7fb63584fe8f08074fcd41fd66
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-04-22 12:24:51 +0530

    Time: 6 ms (32.11%) | Memory: 31 MB (27.88%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,19 +1,19 @@
 class Solution {
-    int dir[4][2]={{1,0},{-1,0},{0,1},{0,-1}};
+    vector<pair<int,int>> dir={{1,0},{0,1},{-1,0},{0,-1}};
 public:
-    int bfs(vector<vector<int>>& grid,int row,int col){
-        queue<pair<int,int>>q;
-        grid[row][col]=0;
-        q.push({row,col});
+    int bfs(int i,int j,vector<vector<int>>& grid){
+        queue<pair<int,int>> q;
+        grid[i][j]=0;
+        q.push({i,j});
         int res=1;
         while(!q.empty()){
-            auto node=q.front();
+            auto top=q.front();
             q.pop();
-            int curr_row=node.first;
-            int curr_col=node.second;
-            for(int i=0;i<4;i++){
-                int nr=curr_row+dir[i][0];
-                int nc=curr_col+dir[i][1];
+            int row=top.first;
+            int col=top.second;
+            for(auto& d:dir){
+                int nr=row+d.first;
+                int nc=col+d.second;
                 if(nr>=0 && nc>=0 && nr<grid.size() && nc<grid[0].size() && grid[nr][nc]==1){
                     q.push({nr,nc});
                     grid[nr][nc]=0;
@@ -24,14 +24,12 @@ public:
     return res;
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int area=0;
+        int maxArea=0;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]==1){
-                    area=max(area,bfs(grid,i,j));
-                }
+                if(grid[i][j]==1)maxArea=max(maxArea,bfs(i,j,grid));
             }
         }
-    return area;
+    return maxArea;
     }
 };
