76415e45190d6d236f563aa24fbdc564972d3fe8
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-04-29 15:45:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (86.87%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s1.size()>s2.size()) return false;
+        vector<int> counts1(26,0),countWindow(26,0);
+        for(char c:s1) counts1[c-'a']++;
+        for(int i=0;i<s2.size();i++){
+            countWindow[s2[i]-'a']++;
+
+            if(i>=s1.size()){
+                countWindow[s2[i-s1.size()]-'a']--;
+            }
+
+            if(counts1==countWindow)return true;
+        }
+    return false;
+    }
+};
