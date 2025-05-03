42d51236e11b91dd2f031e46de4da86f4760e55b
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-05-03 15:24:30 +0530

    Time: 198 ms (64.32%) | Memory: 135.3 MB (29.77%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -2,39 +2,37 @@ class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n=points.size();
-        unordered_map<int,vector<pair<int,int>>> adj;
+        unordered_map<int,vector<pair<int,int>>>adj;
         for(int i=0;i<n;i++){
             int x1=points[i][0];
             int y1=points[i][1];
             for(int j=i+1;j<n;j++){
                 int x2=points[j][0];
                 int y2=points[j][1];
-                int dist=abs(x1-x2)+abs(y1-y2);
+                int dist=abs(x2-x1)+abs(y2-y1);
                 adj[i].push_back({dist,j});
                 adj[j].push_back({dist,i});
             }
         }
         int res=0;
-        unordered_set<int>visit;
         priority_queue<pair<int, int>, vector<pair<int, int>>,greater<pair<int, int>>> minH;
+        unordered_set<int>visited;
         minH.push({0,0});
-        while(visit.size()<n){
+        while(visited.size()<n){
             auto curr=minH.top();
             minH.pop();
             int cost=curr.first;
             int node=curr.second;
-            if(visit.count(node)) continue;
+            if(visited.count(node)) continue;
             res+=cost;
-            visit.insert(node);
-
+            visited.insert(node);
             for(const auto& nei:adj[node]){
                 int neiCost=nei.first;
                 int neiNode=nei.second;
-                if(!visit.count(neiNode)){
+                if(!visited.count(neiNode)){
                     minH.push({neiCost,neiNode});
                 }
             }
-            
         }
     return res;
     }
