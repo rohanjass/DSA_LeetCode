242f4bd97a155f914ade60693ef55d92de770a9e
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-05-06 12:37:59 +0530

    Time: 163 ms (28.79%) | Memory: 291.9 MB (12.06%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -4,18 +4,19 @@ public:
         int resLen=0;
         int resInd=-1;
         int n=s.size();
-        vector<vector<bool>>dp(n,vector<bool>(n,false));
+        vector<vector<int>>dp(n,vector<int>(n,0));
         for(int i=n-1;i>=0;i--){
             for(int j=i;j<n;j++){
-                if(s[i]==s[j] &&(j-i<=2 || dp[i+1][j-1])){
+                if(s[i]==s[j] && (j-i<=2 || dp[i+1][j-1])){
                     dp[i][j]=true;
                     if(resLen<(j-i+1)){
-                        resInd=i;
                         resLen=j-i+1;
+                        resInd=i;
                     }
                 }
             }
         }
     return s.substr(resInd,resLen);
+
     }
 };
