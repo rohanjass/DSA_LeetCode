f2564fad33e08e5cb6c902bf6dce19742f5fec7e
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-05-09 13:29:18 +0530

    Time: 23 ms (68.33%) | Memory: 17.9 MB (69.74%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -6,8 +6,10 @@ public:
         for(int i=1;i<=amount;i++){
             for(int j=0;j<coins.size();j++){
                 if(coins[j]<=i) dp[i]=min(dp[i],dp[i-coins[j]]+1);
+                //dp[i-coins[j]]+1---> taking current( +1 ) coin and looking back to find balance amount( dp[i-coins[j]] );
             }
         }
     return dp[amount]>amount? -1:dp[amount];
+    //we intialized evryone to amount+1 so id fp[amount]>amount that means no combination is founded!!
     }
 };
