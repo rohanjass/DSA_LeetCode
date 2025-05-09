e55c607d7cb92b5b836b09473924468e2449c110
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-05-09 12:49:26 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (47.51%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -11,14 +11,14 @@
  */
 class Solution {
 public:
-    bool dfs(TreeNode* node,long lower,long upper){
+    bool dfs(TreeNode* node,long lower,long higher){
         if(node==nullptr) return true;
-        if(!(lower<node->val && node->val<upper)) return false;
-        return dfs(node->right,node->val,upper) && dfs(node->left,lower,node->val);
+        if(!(lower<node->val && node->val<higher)) return false;
+        return dfs(node->left,lower,node->val) && dfs(node->right,node->val,higher);
     }
     bool isValidBST(TreeNode* root) {
         long lower=LONG_MIN;
-        long upper=LONG_MAX;
-        return dfs(root,lower,upper);
+        long higher=LONG_MAX;
+        return dfs(root,lower,higher);
     }
 };
