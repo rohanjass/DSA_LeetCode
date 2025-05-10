415fd660d3e0c2a9fe647a24cf50121996e70125
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-05-10 14:35:39 +0530

    Time: 3 ms (27.12%) | Memory: 13.9 MB (90.04%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    void capture(vector<vector<char>>& board,int r,int c){
-        if(r<0 || c<0 || r>=board.size() || c>=board[0].size() || board[r][c]!='O') return;
-        board[r][c]='T';
-        capture(board,r+1,c);
-        capture(board,r-1,c);
-        capture(board,r,c+1);
-        capture(board,r,c-1);
+    void dfs(int i,int j,vector<vector<char>>& board){
+        if(i<0 ||j<0 || i>=board.size() || j>=board[0].size() || board[i][j]!='O') return;
+        board[i][j]='T';
+        dfs(i+1,j,board);
+        dfs(i-1,j,board);
+        dfs(i,j+1,board);
+        dfs(i,j-1,board);
     }
-
     void solve(vector<vector<char>>& board) {
-        int row=board.size();
-        int col=board[0].size();
-        for(int r=0;r<row;r++){
-            if(board[r][0]=='O') capture(board,r,0);
-            if(board[r][col-1]=='O') capture(board,r,col-1);
+        int r=board.size();
+        int c=board[0].size();
+        for(int i=0;i<r;i++){
+            if(board[i][0]=='O') dfs(i,0,board);
+            if(board[i][c-1]=='O') dfs(i,c-1,board);
         }
-        for(int c=0;c<col;c++){
-            if(board[0][c]=='O') capture(board,0,c);
-            if(board[row-1][c]=='O') capture(board,row-1,c);
+        for(int i=0;i<c;i++){
+            if(board[0][i]=='O') dfs(0,i,board);
+            if(board[r-1][i]=='O') dfs(r-1,i,board);
         }
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
                 if(board[i][j]=='T') board[i][j]='O';
                 else if(board[i][j]=='O') board[i][j]='X';
             }
