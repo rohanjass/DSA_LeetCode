706a1ae4be83dcc64ee0dc20e11a41371db3f55d
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-07-04 17:46:59 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (73.50%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    void dfs(int i,vector<int>& arr, int target,vector<vector<int>>& res,vector<int>& path){
+    void dfs(int i,vector<int>& arr, int target,vector<int>& path,vector<vector<int>>& res){
         if(i>=arr.size()){
             if(target==0){
                 res.push_back(path);
             }
-        return;
+            return;
         }
         if(arr[i]<=target){
             path.push_back(arr[i]);
-            dfs(i,arr,target-arr[i],res,path);
+            dfs(i,arr,target-arr[i],path,res);
             path.pop_back();
         }
-        dfs(i+1,arr,target,res,path);
+        dfs(i+1,arr,target,path,res);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> res;
         vector<int> path;
-        dfs(0,candidates,target,res,path);
+        dfs(0,candidates,target,path,res);
         return res;
     }
 };
