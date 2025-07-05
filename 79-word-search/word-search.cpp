ed50980ea67a53f059b36c7b5a563d1eb850dc9b
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-07-05 12:26:08 +0530

    Time: 890 ms (13.65%) | Memory: 11.7 MB (24.54%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    bool dfs(int ind,int i,int j,vector<vector<char>>& board, string word){
+    bool dfs(vector<vector<char>>& board, string word,int r,int c,int ind){
         if(ind==word.size()) return true;
-        if(i<0 || j<0 || i>=board.size() || j>=board[0].size() || board[i][j]=='!' || board[i][j]!=word[ind]) return false;
-        char c=board[i][j];
-        board[i][j]='!';
-        bool left = dfs(ind+1,i+1,j,board,word);
-        bool right = dfs(ind+1,i,j+1,board,word);
-        bool up = dfs(ind+1,i-1,j,board,word);
-        bool down = dfs(ind+1,i,j-1,board,word);
-        board[i][j]=c;
-        
-        return left || right || up || down;
+        if(r<0 || c<0 || r>=board.size() || c>=board[0].size() || board[r][c]!=word[ind] || board[r][c]=='!') return false;
+        char ch=board[r][c];
+        board[r][c]='!';
+        bool l=dfs(board,word,r+1,c,ind+1);
+        bool ri=dfs(board,word,r,c+1,ind+1);
+        bool u=dfs(board,word,r-1,c,ind+1);
+        bool d=dfs(board,word,r,c-1,ind+1);
+        board[r][c]=ch;
+
+        return l||ri||u||d;
     }
     bool exist(vector<vector<char>>& board, string word) {
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
-                if(dfs(0,i,j,board,word)) return true;
+                if(dfs(board,word,i,j,0)) return true;
             }
         }
     return false;
