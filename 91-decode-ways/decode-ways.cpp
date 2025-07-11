0f00d6aebb91267d8208563c1991fd77f65b6ce3
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-07-11 15:03:02 +0530

    Time: 1 ms (31.60%) | Memory: 10.9 MB (17.63%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    int numDecodings(string s) {
-        vector<int>dp(s.size()+1);
-        dp[s.size()]=1;
-        for(int i=s.size()-1;i>=0;i--){
-            if(s[i]=='0'){
-                dp[i]=0;
-            } else {
-                dp[i]=dp[i+1];//taking one element 
-                if(i+1<s.size() &&(s[i]=='1' || (s[i]=='2' && s[i+1]<'7'))){
-                    dp[i]+=dp[i+2];//taking two elements
-                }
-            }
+    int dfs(int i,string s,vector<int>& dp){
+        if(i==s.size()) return 1;
+        if(s[i]=='0') return 0;
+        if(dp[i]!=-1) return dp[i];
+        int res=dfs(i+1,s,dp);
+        if(i+1<s.size() && (s[i]=='1'||(s[i]=='2' && s[i+1]<'7'))){
+            res+=dfs(i+2,s,dp);
         }
-    return dp[0];
+    return dp[i]=res;
+    }
+    int numDecodings(string s) {
+        vector<int> dp(s.size(),-1);
+        return dfs(0,s,dp);
     }
 };
