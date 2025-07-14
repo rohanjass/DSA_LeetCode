725813ceb7ddcbb3359bb34b3725164701337307
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-07-14 19:17:28 +0530

    Time: 5 ms (35.74%) | Memory: 19.3 MB (17.36%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -2,25 +2,23 @@ class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         vector<vector<pair<int,int>>>adj(n);
-        vector<vector<int>> dist(n,vector<int>(k+5,1e9));
-        for(auto& flight:flights){
-            adj[flight[0]].push_back({flight[1],flight[2]});
+        vector<vector<int>>dist(n,vector<int>(k+5,1e9));
+        priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<tuple<int,int,int>>> minH;
+        for(auto& f:flights){
+            adj[f[0]].push_back({f[1],f[2]});
         }
-        priority_queue<tuple<int,int,int>, 
-                       vector<tuple<int,int,int>>, 
-                       greater<tuple<int,int,int>>> minHeap;
-        minHeap.push({0,src,-1});
+        minH.push({0,src,-1});//{cost,flight,stopCount}
         dist[src][0]=0;
-        while(!minHeap.empty()){
-            auto [cost,flight,stops]=minHeap.top();
-            minHeap.pop();
+        while(!minH.empty()){
+            auto [cost,flight,stops]=minH.top();
+            minH.pop();
             if(flight==dst) return cost;
             if(stops==k || dist[flight][stops+1]<cost) continue;
-            for(const auto&[nei,nxtCst]:adj[flight]){
-                int nextStops=stops+1;
-                if(dist[nei][nextStops+1]>cost+nxtCst){
-                    dist[nei][nextStops+1]=cost+nxtCst;
-                    minHeap.push({cost+nxtCst,nei,stops+1});
+            for(auto& [neig,nextCost]:adj[flight]){
+                int nextStop=stops+1;
+                if(dist[neig][nextStop+1]>cost+nextCost){
+                    dist[neig][nextStop+1]=cost+nextCost;
+                    minH.push({cost+nextCost,neig,nextStop});
                 }
             }
         }
