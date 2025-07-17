c1d3d2df6ae5730508bed6e34d63e360e243e90c
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-07-17 13:24:38 +0530

    Time: 95 ms (27.35%) | Memory: 45.4 MB (47.90%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -3,28 +3,26 @@ public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         int s=times.size();
         unordered_map<int,vector<pair<int,int>>>adj;
-        for(const auto& time:times){
-            adj[time[0]].emplace_back(make_pair(time[1],time[2]));
+        for(const auto& t:times){
+            adj[t[0]].push_back({t[1],t[2]});
         }
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minHeap;
-        minHeap.push({0,k});
-        set<int>visited;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minH;
+        minH.push({0,k});
+        set<int> visited;
         int t=0;
-        while(!minHeap.empty()){
-            auto curr=minHeap.top();
-            minHeap.pop();
-            int w1=curr.first;
+        while(!minH.empty()){
+            auto curr=minH.top();
+            minH.pop();
+            int t1=curr.first;
             int n1=curr.second;
             if(visited.count(n1)) continue;
             visited.insert(n1);
-            t=w1;
-            if(adj.count(n1)){
-                for(const auto& nei:adj[n1]){
-                    int n2=nei.first;
-                    int w2=nei.second;
-                    if(!visited.count(n2)){
-                        minHeap.push({w1+w2,n2});
-                    }
+            t=t1;
+            for(const auto& nei:adj[n1]){
+                int n2=nei.first;
+                int t2=nei.second;
+                if(!visited.count(n2)){
+                    minH.push({t1+t2,n2});
                 }
             }
         }
