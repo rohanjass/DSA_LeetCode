d65bcb650a7ed19e6375f5162ef4034fccd82536
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-07-21 14:35:32 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (75.28%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        int fresh=0;
         queue<pair<int,int>>q;
-        int fresh=0,time=0;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]==1) fresh++;
-                if(grid[i][j]==2) q.push({i,j});
+                if(grid[i][j]==2) q.push({i,j}); 
             }
         }
-        vector<pair<int,int>>dir={{1,0},{0,1},{-1,0},{0,-1}};
+        vector<pair<int,int>> dir={{1,0},{0,1},{-1,0},{0,-1}};
+        int time=0;
         while(!q.empty() && fresh){
             int len=q.size();
             for(int i=0;i<len;i++){
-                auto cur=q.front();
+                auto curr= q.front();
                 q.pop();
-                int row=cur.first;
-                int col=cur.second;
-
-                for(auto& d:dir){
-                    int nr=row+d.first;
-                    int nc=col+d.second;
-                    if(nc>=0 && nr>=0 && nr<grid.size() && nc<grid[0].size() && grid[nr][nc]==1){
-                        grid[nr][nc]=2;
-                        q.push({nr,nc});
-                        fresh--;
-                    }
+                int r=curr.first;
+                int c=curr.second;
+                for(const auto& d:dir){
+                    int nr=r+d.first;
+                    int nc=c+d.second;
+                    if(nr<0 || nc<0 || nr>=grid.size() || nc >=grid[0].size() || grid[nr][nc]==0 || grid[nr][nc]==2) continue;
+                    fresh--;
+                    grid[nr][nc]=2;
+                    q.push({nr,nc});
                 }
             }
             time++;
         }
-        return (fresh==0)? time:-1;
+    return (fresh==0)? time:-1;
     }
 };
