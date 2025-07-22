f2ecc3c5017c3b6ffe3f886adfa706151f0b5783
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-07-22 12:29:36 +0530

    Time: 3 ms (20.58%) | Memory: 11 MB (15.79%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -5,13 +5,13 @@ public:
         if(s[i]=='0') return 0;
         if(dp[i]!=-1) return dp[i];
         int res=dfs(i+1,s,dp);
-        if(i+1<s.size() && (s[i]=='1'||(s[i]=='2' && s[i+1]<'7'))){
+        if(i+1<s.size() && (s[i]=='1'|| (s[i]=='2' && s[i+1]<'7'))){
             res+=dfs(i+2,s,dp);
         }
-    return dp[i]=res;
+        return dp[i]=res;
     }
     int numDecodings(string s) {
-        vector<int> dp(s.size(),-1);
+        vector<int> dp(s.size()+1,-1);
         return dfs(0,s,dp);
     }
 };
