dd96adfeccafa9407872c42a5fae416b84bfee18
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-07-29 11:04:51 +0530

    Time: 49 ms (57.42%) | Memory: 57.8 MB (55.71%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool checkPal(string s,int start,int end){
+        while(start<=end){
+            if(s[start]!=s[end]) return false;
+            start++;
+            end--;
+        }
+    return true;
+    }
+    void dfs(int ind,string& s,vector<string>& path,vector<vector<string>>& res){
+        if(ind==s.size()){
+            res.push_back(path);
+        return;
+        }
+        for(int i=ind;i<s.size();i++){
+            if(checkPal(s,ind,i)){
+                path.push_back(s.substr(ind,i-ind+1));
+                dfs(i+1,s,path,res);
+                path.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> res;
+        vector<string> path;
+        dfs(0,s,path,res);
+        return res;
+    }
+};
