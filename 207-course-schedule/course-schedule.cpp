8e1b881d209aa5b9022d4f3224523b6458b50ba3
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-08-08 14:50:47 +0530

    Time: 7 ms (48.83%) | Memory: 21 MB (11.94%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,25 +1,33 @@
 class Solution {
-    unordered_map<int,vector<int>>mp;
-    unordered_set<int> visiting;
 public:
+    unordered_map<int,vector<int>> mp;
+    unordered_set<int> visiting;
+
     bool dfs(int crs){
-        if(visiting.count(crs)) return false;
-        if(mp[crs].empty()) return true;
+        if(visiting.count(crs)){
+            return false;
+        }
+        if(mp[crs].empty()){
+            return true;
+        }
         visiting.insert(crs);
-        for(int pre:mp[crs]){
-            if(!dfs(pre)) return false;
+        for(int i:mp[crs]){
+            if(!dfs(i)) return false;
         }
         visiting.erase(crs);
         mp[crs]={};
-        return true;
+    return true;
     }
+
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         for(int i=0;i<numCourses;i++){
             mp[i]={};
         }
+
         for(const auto& p:prerequisites){
             mp[p[0]].push_back(p[1]);
         }
+
         for(int i=0;i<numCourses;i++){
             if(!dfs(i)) return false;
         }
