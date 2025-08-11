e6dfa02c23fb3155d2a21fbd284db6d89aa4171a
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-08-11 14:49:38 +0530

    Time: 12 ms (59.23%) | Memory: 20.5 MB (92.14%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> dirs={{1,0},{-1,0},{0,1},{0,-1}};
-    vector<vector<int>>dp;
-    int dfs(vector<vector<int>>& matrix,int row,int col,int preVal){
-        if(row<0 || col<0 || row>=matrix.size() || col>=matrix[0].size() || matrix[row][col]<=preVal) return 0;
-        if(dp[row][col]!=-1) return dp[row][col];
-        int res=1;
-        for(auto d:dirs){
-            int nr=row+d[0];
-            int nc=col+d[1];
-            res=max(res,1+dfs(matrix,nr,nc,matrix[row][col]));
+    vector<pair<int,int>> dir={{1,0},{0,1},{-1,0},{0,-1}};
+    vector<vector<int>> dp;
+    int dfs(vector<vector<int>>& matrix,int r,int c,int preVal){
+        if(r<0 || c<0 || r>=matrix.size() || c>=matrix[0].size() || matrix[r][c]<=preVal) return 0;
+        if(dp[r][c]!=-1) return dp[r][c];
+        int  res=1;
+        for(const auto& d:dir){
+            int nr=r+d.first;
+            int nc=c+d.second;
+            res=max(res,1+dfs(matrix,nr,nc,matrix[r][c]));
         }
-        dp[row][col]=res;
-    return res;
+    return dp[r][c]=res;
     }
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         int row=matrix.size(),col=matrix[0].size();
-        dp=vector<vector<int>>(row,vector<int>(col,-1));
-        int LIP=-1;
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                LIP=max(LIP,dfs(matrix,i,j,INT_MIN));
+        dp=vector<vector<int>> (row,vector<int>(col,-1));
+        int res=-1;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                res=max(res,dfs(matrix,i,j,INT_MIN));
             }
         }
-    return LIP;
+    return res;
     }
 };
