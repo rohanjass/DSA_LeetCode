58db3cc864d051202e442820045afb564a242cd0
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-08-11 14:27:52 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (20.87%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        vector<int>nums1(nums.begin()+1,nums.end());
-        vector<int>nums2(nums.begin(),nums.end()-1);
-        return max(nums[0],max(helper(nums1),helper(nums2)));
+    int dfs(int i,int n,vector<int>& nums,vector<int>& dp){
+        if(i>n) return 0;
+
+        if(dp[i]!=-1) return dp[i];
+        int take=nums[i]+dfs(i+2,n,nums,dp);
+        int notTake=dfs(i+1,n,nums,dp);
+
+        return dp[i]=max(take,notTake);
     }
-    int helper(vector<int>& nums){
-        int rob1=0,rob2=0;
-        for(int rob:nums){
-            int temp=max(rob+rob1,rob2);
-            rob1=rob2;
-            rob2=temp;
-        }
-    return max(rob1,rob2);
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1) return nums[0];
+        vector<int> dp1(nums.size()+1,-1);
+        vector<int> dp2(nums.size()+1,-1);
+
+        int op1=dfs(0,n-2,nums,dp1);
+        int op2=dfs(1,n-1,nums,dp2);
+    return max(op1,op2);
     }
 };
