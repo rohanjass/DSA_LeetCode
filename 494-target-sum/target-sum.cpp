ef1f33a68124cdce6afef14785be8972d2d4c58e
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-08-21 14:21:27 +0530

    Time: 121 ms (39.82%) | Memory: 50 MB (15.68%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
-        vector<unordered_map<int,int>>dp(n+1);
+        vector<unordered_map<int,int>> dp(n+1);
         dp[0][0]=1;
-
         for(int i=0;i<n;i++){
             for(auto& p:dp[i]){
                 int sum=p.first,ways=p.second;
