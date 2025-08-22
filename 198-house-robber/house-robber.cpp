bbcf23e40c5279f9a0266411e25f54f4e8414788
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-08-22 12:33:16 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (22.76%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -3,13 +3,14 @@ public:
     int dfs(int i,vector<int>& nums,vector<int>& dp){
         if(i>=nums.size()) return 0;
         if(dp[i]!=-1) return dp[i];
+
         int take=nums[i]+dfs(i+2,nums,dp);
         int notTake=dfs(i+1,nums,dp);
 
         return dp[i]=max(take,notTake);
     }
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size()+1,-1);
+        vector<int> dp(nums.size(),-1);
         return dfs(0,nums,dp);
     }
 };
