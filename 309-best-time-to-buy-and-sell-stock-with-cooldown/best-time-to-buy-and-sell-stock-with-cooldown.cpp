970d1adf3e6eedaad1f0deb108bc64c4ea4066d3
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-08-25 12:39:13 +0530

    Time: 6 ms (10.72%) | Memory: 16.4 MB (13.54%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int dfs(int i,int buying,vector<int>& prices,vector<vector<int>>& dp){
+    int dfs(int i,int buying ,vector<int>& prices,vector<vector<int>>& dp){
         if(i>=prices.size()) return 0;
         if(dp[i][buying]!=-1) return dp[i][buying];
 
-        int coolDown=dfs(i+1,buying,prices,dp);
+        int cooldown=dfs(i+1,buying,prices,dp);
         if(buying){
             int buy=dfs(i+1,0,prices,dp)-prices[i];
-            dp[i][buying]=max(buy,coolDown);
+            dp[i][buying]=max(buy,cooldown);
         } else {
-            int sell=dfs(i+2,1,prices,dp)+prices[i];
-            dp[i][buying]=max(sell,coolDown);
+            int sell=dfs(i+2,1,prices,dp)+prices[i];//i+2 bcoz there is compulsory cooldown after sell ,so directly calling i+2
+            dp[i][buying]=max(sell,cooldown);
         }
     return dp[i][buying];
     }
