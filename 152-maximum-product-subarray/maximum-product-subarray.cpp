f8305871d577dac1882c63b4bf242bdcc4a8b273
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-08-26 20:09:22 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (84.61%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int res=nums[0];
-        int currMin=1,currMax=1;
+        int Max=1,Min=1;
         for(int num:nums){
-            int temp=currMax*num;
-            currMax=max(max(currMax*num,currMin*num),num);
-            currMin=min(min(temp,currMin*num),num);
-            res=max(currMax,res);
+            int temp=Max*num;
+            Max=max(max(Max*num,Min*num),num);
+            Min=min(min(temp,Min*num),num);
+            res=max(res,Max);
         }
     return res;
     }
