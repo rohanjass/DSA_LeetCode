7f9f1d8004ee95422103b0bbafef7861f3387284
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-08-27 14:02:55 +0530

    Time: 26 ms (14.45%) | Memory: 16.9 MB (14.04%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
-        int n=grid.size();
-        set<pair<int,int>> visited;
-        priority_queue<vector<int>,vector<vector<int>>,greater<>>minH;
         vector<pair<int,int>> dir={{1,0},{0,1},{-1,0},{0,-1}};
+        int n=grid.size();
+        priority_queue<vector<int>,vector<vector<int>>,greater<>>minH;
+        set<pair<int,int>> visited;
         minH.push({grid[0][0],0,0});
         visited.insert({0,0});
         while(!minH.empty()){
-            auto curr=minH.top();
+            auto cur=minH.top();
             minH.pop();
-            int time=curr[0];
-            int r=curr[1];
-            int c=curr[2];
-            if(r==n-1 && c==n-1) return time;
+            int node=cur[0];
+            int row=cur[1];
+            int col=cur[2];
+            if(row==n-1 && col==n-1) return node;
             for(const auto& d:dir){
-                int nr=r+d.first;
-                int nc=c+d.second;
+                int nr=row+d.first;
+                int nc=col+d.second;
                 if(nr<0 || nc<0 || nr>=n || nc>=n || visited.count({nr,nc})) continue;
-                visited.insert({nr,nc});
-                minH.push({max(time,grid[nr][nc]),nr,nc});
+                minH.push({max(node,grid[nr][nc]),nr,nc});
+                visited.insert({nr,nc}); 
             }
         }
     return -1;
