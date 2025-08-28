5894870e03ccbc6ed75392645a714f2e4ac4a177
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-08-28 15:48:11 +0530

    Time: 3 ms (88.75%) | Memory: 22.1 MB (94.70%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,34 +1,34 @@
 class Solution {
-    vector<pair<int,int>>dir={{1,0},{0,1},{-1,0},{0,-1}};
 public:
-    void dfs(int r,int c,vector<vector<bool>>& oc,vector<vector<int>>& arr){
+    vector<pair<int,int>> dir={{1,0},{0,1},{-1,0},{0,-1}};
+    void dfs(vector<vector<bool>>& oc,int r,int c,vector<vector<int>>& a){
         oc[r][c]=true;
-        for(auto& d:dir){
+        for(const auto& d:dir){
             int nr=r+d.first;
             int nc=c+d.second;
-            if(nr>=0 && nc>=0 && nr<arr.size() && nc<arr[0].size() && oc[nr][nc]==false && arr[nr][nc]>=arr[r][c]){
-                dfs(nr,nc,oc,arr);
+            if(nr>=0 && nc>=0 && nr<a.size() && nc<a[0].size() && oc[nr][nc]==false && a[r][c]<=a[nr][nc]){
+                dfs(oc,nr,nc,a);
             }
         }
+    
     }
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        int m=heights.size(),n=heights[0].size();
+        vector<vector<bool>>pcf(m,vector<bool>(n,false));
+        vector<vector<bool>>atl(m,vector<bool>(n,false));
+        for(int i=0;i<m;i++){
+            dfs(pcf,i,0,heights);
+            dfs(atl,i,n-1,heights);
+        }
+        for(int i=0;i<n;i++){
+            dfs(pcf,0,i,heights);
+            dfs(atl,m-1,i,heights);
+        }
+
         vector<vector<int>> res;
-        int ROW=heights.size(),COL=heights[0].size();
-        vector<vector<bool>>pcf(ROW,vector<bool>(COL,false));
-        vector<vector<bool>>atl(ROW,vector<bool>(COL,false));
-        for(int r=0;r<ROW;r++){
-            dfs(r,0,pcf,heights);//left pcf oc
-            dfs(r,COL-1,atl,heights);//right atl oc
-        }
-        for(int c=0;c<COL;c++){
-            dfs(0,c,pcf,heights);//top pcf oc
-            dfs(ROW-1,c,atl,heights);//bottom atl oc
-        }
-        for(int i=0;i<ROW;i++){
-            for(int j=0;j<COL;j++){
-                if(pcf[i][j] && atl[i][j]){
-                    res.push_back({i,j});
-                }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(pcf[i][j] && atl[i][j]) res.push_back({i,j});
             }
         }
     return res;
