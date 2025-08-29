e4ca3d2a21fdaa06bf53105073f4cb3d97ed2037
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-08-29 11:51:50 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (80.61%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-    int dfs(vector<int>& nums,int i,bool flag,vector<vector<int>>& dp){
-        if(i==nums.size()) return flag? 0:-1e6;
-        int f=flag? 1:0;
-        if(dp[i][f]!=INT_MIN) return dp[i][f];
-        if(flag) 
-            dp[i][f]= max(0,nums[i]+dfs(nums,i+1,true,dp));
-        else
-            dp[i][f]=max(dfs(nums,i+1,false,dp),nums[i]+dfs(nums,i+1,true,dp));
-    return dp[i][f];
-    }
     int maxSubArray(vector<int>& nums) {
-        vector<vector<int>>dp(nums.size()+1,vector<int>(2,INT_MIN));
-        return dfs(nums,0,false,dp);
+        int maxSub=nums[0],currSum=0;
+        for(int i=0;i<nums.size();i++){
+            if(currSum<0){
+                currSum=0;
+            }
+            currSum+=nums[i];
+            maxSub=max(maxSub,currSum);
+        }
+    return maxSub;
     }
 };
