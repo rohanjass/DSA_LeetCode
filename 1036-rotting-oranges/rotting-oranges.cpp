e57fae474d03b0c21ee6f6f3326cefceac563ff4
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-09-24 11:02:34 +0530

    Time: 3 ms (28.88%) | Memory: 16.8 MB (75.20%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
         int fresh=0;
-        queue<pair<int,int>>q;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
+        queue<pair<int,int>> q;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]==1) fresh++;
-                if(grid[i][j]==2) q.push({i,j}); 
+                if(grid[i][j]==2) q.push({i,j});
             }
-        }
+        } 
+
         vector<pair<int,int>> dir={{1,0},{0,1},{-1,0},{0,-1}};
+
         int time=0;
+
         while(!q.empty() && fresh){
             int len=q.size();
             for(int i=0;i<len;i++){
-                auto curr= q.front();
+                auto node=q.front();
                 q.pop();
-                int r=curr.first;
-                int c=curr.second;
+                int r=node.first;
+                int c=node.second;
                 for(const auto& d:dir){
                     int nr=r+d.first;
                     int nc=c+d.second;
-                    if(nr<0 || nc<0 || nr>=grid.size() || nc >=grid[0].size() || grid[nr][nc]==0 || grid[nr][nc]==2) continue;
+
+                    if(nr<0 || nc<0 || nr>=m || nc>=n || grid[nr][nc]==2 || grid[nr][nc]==0) continue;
                     fresh--;
                     grid[nr][nc]=2;
                     q.push({nr,nc});
                 }
             }
-            time++;
+        time++;
         }
     return (fresh==0)? time:-1;
     }
