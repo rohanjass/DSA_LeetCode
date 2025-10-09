0664fbe34d31389eb936eb066e811ad805e4d516
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-10-09 15:27:49 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (53.32%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        vector<int>count(26,0);
+        priority_queue<int> maxH;
+        vector<int> count(26,0);
+        queue<pair<int,int>>q;//{cnt,time}
         for(char c:tasks){
             count[c-'A']++;
         }
-        priority_queue<int>maxH;
         for(int i:count){
-            if(i>0)maxH.push(i);
+            if(i>0) maxH.push(i);
         }
-        queue<pair<int,int>> q;
         int time=0;
-
         while(!maxH.empty() || !q.empty()){
             time++;
             if(maxH.empty()){
@@ -30,3 +29,8 @@ public:
     return time;
     }
 };
+
+
+//maxH contain the tasks that we can use imediately based on their ant priority 
+// if A is scheduled to be used on time 7 we push it to priority queue on time 6 bcoz on next iteration time 7 will be updated and we can pop it from the PQ and use it
+//use of the queue is to schedule the task ; when it will go to the PQ
