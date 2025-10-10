4c026fbcd8c9d822366876ee76ac5a7a2a56e7c9
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-10-10 11:43:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (72.14%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int dfs(int i,string s,vector<int>& dp){
+    int def(int i,string& s,vector<int>& dp){
         if(i==s.size()) return 1;
         if(s[i]=='0') return 0;
         if(dp[i]!=-1) return dp[i];
-        int res=dfs(i+1,s,dp);
-        if(i+1<s.size() && (s[i]=='1'|| (s[i]=='2' && s[i+1]<'7'))){
-            res+=dfs(i+2,s,dp);
+        int res=def(i+1,s,dp);
+        if(i+1<s.size() && (s[i]=='1' || ( s[i]=='2' && s[i+1]<'7'))){
+            res+=def(i+2,s,dp);
         }
-        return dp[i]=res;
+    return dp[i]=res;
     }
     int numDecodings(string s) {
-        vector<int> dp(s.size()+1,-1);
-        return dfs(0,s,dp);
+        int n=s.size();
+        vector<int>dp(n,-1);
+        return def(0,s,dp);
     }
 };
