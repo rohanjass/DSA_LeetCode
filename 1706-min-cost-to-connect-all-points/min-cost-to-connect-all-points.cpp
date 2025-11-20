94245d8cae64d321c08fa55adc4898a41f45eb9d
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-11-20 16:43:48 +0530

    Time: 191 ms (63.73%) | Memory: 135.5 MB (28.33%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-        unordered_map<int,vector<pair<int,int>>>adj;
+        unordered_map<int,vector<pair<int,int>>> adj;
         unordered_set<int> visited;
-        priority_queue<pair<int, int>, vector<pair<int, int>>,greater<pair<int, int>>> minH;
         int n=points.size();
         for(int i=0;i<n;i++){
             int x1=points[i][0];
@@ -11,27 +10,31 @@ public:
             for(int j=i+1;j<n;j++){
                 int x2=points[j][0];
                 int y2=points[j][1];
-                int dist=abs(x2-x1)+abs(y2-y1);
+                int dist=abs(x1-x2)+abs(y1-y2);
                 adj[i].push_back({dist,j});
                 adj[j].push_back({dist,i});
             }
         }
-
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> minH;
         minH.push({0,0});
         int res=0;
+
         while(visited.size()<n){
             auto curr=minH.top();
             minH.pop();
             int dist=curr.first;
             int node=curr.second;
-            if(visited.count(node))continue;
+
+            if(visited.count(node)) continue;
             res+=dist;
             visited.insert(node);
-            for(const auto& i:adj[node]){
-                int nDist=i.first;
-                int nNode=i.second;
-                if(!visited.count(nNode)){
-                    minH.push({nDist,nNode});
+
+            for(const auto& N:adj[node]){
+                int newDist=N.first;
+                int newNode=N.second;
+
+                if(!visited.count(newNode)){
+                    minH.push({newDist,newNode});
                 }
             }
         }
