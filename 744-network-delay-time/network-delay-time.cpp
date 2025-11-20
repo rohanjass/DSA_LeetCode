68fa0bc20ffad9847fe34391504db4aff3c40634
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-11-20 12:59:55 +0530

    Time: 94 ms (28.13%) | Memory: 45.4 MB (45.81%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        int s=times.size();
         unordered_map<int,vector<pair<int,int>>> adj;
-        for(const auto& t:times){
+
+        for(auto& t:times){
             adj[t[0]].push_back({t[1],t[2]});
         }
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minH;
+        unordered_set<int> visited;
+
         minH.push({0,k});
-        set<int> visited;
         int t=0;
+
         while(!minH.empty()){
             auto curr=minH.top();
             minH.pop();
             int time=curr.first;
             int node=curr.second;
+
             if(visited.count(node)) continue;
+
             visited.insert(node);
+
             t=time;
-            for(const auto& nei:adj[node]){
-                int newNode=nei.first;
-                int newTime=nei.second;
+
+            for(auto& n:adj[node]){
+                int newNode=n.first;
+                int newTime=n.second;
+
                 if(!visited.count(newNode)){
                     minH.push({time+newTime,newNode});
                 }
