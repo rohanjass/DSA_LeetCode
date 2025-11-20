be56b195500152922ba63c32cf56322ee066b34f
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-11-20 17:13:22 +0530

    Time: 31 ms (9.09%) | Memory: 16.9 MB (11.22%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -3,26 +3,26 @@ public:
     int swimInWater(vector<vector<int>>& grid) {
         int n=grid.size();
         vector<pair<int,int>> dir={{1,0},{0,1},{-1,0},{0,-1}};
-        priority_queue<vector<int>,vector<vector<int>>,greater<>>minH;
+        priority_queue<vector<int>,vector<vector<int>>,greater<>> minH;
         set<pair<int,int>> visited;
         minH.push({grid[0][0],0,0});
         visited.insert({0,0});
+
         while(!minH.empty()){
             auto curr=minH.top();
             minH.pop();
+
             int node=curr[0];
             int row=curr[1];
             int col=curr[2];
 
             if(row==n-1 && col==n-1) return node;
 
-            for(const auto& d:dir){
+            for(auto& d:dir){
                 int nr=row+d.first;
                 int nc=col+d.second;
 
-                if(nr<0 || nc<0 || nr==n || nc==n || visited.count({nr,nc})){
-                    continue;
-                } 
+                if(nr<0 || nc<0 || nr>=n || nc>=n || visited.count({nr,nc})) continue;
 
                 minH.push({max(node,grid[nr][nc]),nr,nc});
                 visited.insert({nr,nc});
