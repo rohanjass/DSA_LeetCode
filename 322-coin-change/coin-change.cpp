2becb820e850285d7c0ba649602e043eb82bdfd0
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-11-21 12:59:36 +0530

    Time: 59 ms (15.66%) | Memory: 53.9 MB (8.93%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    int dfs(vector<int>& coins, int amount,vector<int>& dp){
+    int def(int i,vector<int>& coins,int amount,vector<vector<int>>& dp){
         if(amount==0) return 0;
-        if(amount<0) return INT_MAX;
-        if(dp[amount]!=-1) return dp[amount];
-        int mini=INT_MAX;
-        for(int i=0;i<coins.size();i++){
-            int ans=dfs(coins,amount-coins[i],dp);
-            if(ans!=INT_MAX){
-                mini=min(mini,1+ans);
-            }
+        if(i>=coins.size()){
+           return 1e8;
         }
-    return dp[amount]=mini;
+        if(dp[i][amount]!=-1) return dp[i][amount];
+        int take=1e8;
+        if(amount-coins[i]>=0){ 
+            take=1+def(i,coins,amount-coins[i],dp);
+        }
+        int notTake=def(i+1,coins,amount,dp);
+
+        return dp[i][amount]=min(take,notTake);
     }
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1,-1);
-        int res=dfs(coins,amount,dp);
-        return (res==INT_MAX)? -1:res;
+        int n=coins.size();
+        vector<vector<int>>dp (n+1,vector<int>(amount+1,-1));
+        int res=def(0,coins,amount,dp);
+        return (res>=1e8)? -1:res;
     }
 };
