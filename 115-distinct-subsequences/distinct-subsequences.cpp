5cfdf27bdff13a8613dca6df4ca2c05087eae81e
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-11-25 14:26:05 +0530

    Time: 35 ms (29.73%) | Memory: 27 MB (61.87%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    int dfs(int i,int j,const string& s,const  string& t,vector<vector<int>>& dp){
+    int def(int i,int j,string& s, string& t,vector<vector<int>>& dp){
         if(j==t.size()) return 1;
-        if(i==s.size()) return 0;
+        if(i==s.size()){
+            return 0;
+        }
         if(dp[i][j]!=-1) return dp[i][j];
         long long res=0;
-        if(s[i]==t[j]){
-            res+=dfs(i+1,j+1,s,t,dp);
-        }
-        res+=dfs(i+1,j,s,t,dp);
-    return dp[i][j]=res;
+        if(s[i]==t[j]) res+=def(i+1,j+1,s,t,dp);
+        res+=def(i+1,j,s,t,dp);
+
+        return dp[i][j]=res;
     }
     int numDistinct(string s, string t) {
-        vector<vector<int>> dp(s.size(),vector<int>(t.size(),-1));
-        return dfs(0,0,s,t,dp);
+        int s1=s.size(),s2=t.size();
+        vector<vector<int>> dp(s1,vector<int>(s2,-1));
+        return def(0,0,s,t,dp);
     }
 };
