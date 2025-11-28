f03e0b963fb044d0118ab8dfdcc12590d28a3f13
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-11-28 15:01:26 +0530

    Time: 146 ms (7.22%) | Memory: 54.9 MB (6.13%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    bool def(int i,int j,string& s,string& p,vector<vector<int>>& dp){
-        int n=s.size();
-        int m=p.size();
-        if(i==n && j==m) return true;
-        if(j==m) return false;
-        if (i == n) {
-            for (int k = j; k < m; k++) {
-                if (p[k] != '*') return false;
-            }
-            return true;
-        }
+    bool def(int i,int j,string& s, string& p,vector<vector<int>>& dp){
+        int m=s.size(),n=p.size();
+        if(i==m && j==n) return true;
+        if(j==n) return false;
         if(dp[i][j]!=-1) return dp[i][j];
+        if(i==m){
+            for(int k=j;k<n;k++){
+                if(p[k]!='*') return false;
+            }
+        return true;
+        }
         bool res=false;
         if(s[i]==p[j] || p[j]=='?'){
             res=def(i+1,j+1,s,p,dp);
         } else if(p[j]=='*'){
-            res= def(i+1,j,s,p,dp) || def(i,j+1,s,p,dp);
+            res=(def(i+1,j,s,p,dp) || def(i,j+1,s,p,dp));
         }
     return dp[i][j]=res;
     }
     bool isMatch(string s, string p) {
-        int n=s.size();
-        int m=p.size();
-        vector<vector<int>> dp (n,vector<int>(m,-1));
+        int m=s.size(),n=p.size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
         return def(0,0,s,p,dp);
     }
 };
