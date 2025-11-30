ac514d5c75ce39b4f08e083317d110511507f973
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-11-30 18:22:58 +0530

    Time: 128 ms (55.42%) | Memory: 110.6 MB (24.62%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
+    bool part(int i,int target,vector<int>& nums,vector<vector<int>>& dp){
+        if(target==0) return true;
+        if(i==0) return false;
+        if(dp[i][target]!=-1) return dp[i][target];
+
+        bool notTake=part(i-1,target,nums,dp);
+        bool take=false;
+        if(nums[i]<=target){
+            take=part(i-1,target-nums[i],nums,dp);
+        }
+    return dp[i][target]=notTake || take;
+    }
     bool canPartition(vector<int>& nums) {
-        int sum=0;
-        for(int num:nums){
-            sum+=num;
+        int n=nums.size();
+        int target=0;
+        for(int n:nums){
+            target+=n;
         }
-        if(sum%2!=0) return false;
-        unordered_set<int>dp;
-        dp.insert(0);
-        int target=sum/2;
-    for(int i=nums.size()-1;i>=0;i--){
-            unordered_set<int>nextDp;
-            for(int t:dp){
-                if(t+nums[i]==target) return true;
-                nextDp.insert(t+nums[i]);
-                nextDp.insert(t);
-            }
-            dp=nextDp;
-        }
-    return false;
+        if(target%2!=0) return false;
+        vector<vector<int>> dp(n,vector<int>(target/2+1,-1));
+        return part(n-1,target/2,nums,dp);
     }
 };
