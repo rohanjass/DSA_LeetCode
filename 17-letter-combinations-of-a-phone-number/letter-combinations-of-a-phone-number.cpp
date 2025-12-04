0da6a32d5a16e743aae65f7253441b067f4d874d
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-12-04 11:51:54 +0530

    Time: 2 ms (15.57%) | Memory: 9.1 MB (85.99%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    string mapping[10]={"", "","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    void dfs(int ind,string digits,string path,vector<string>& res){
+    string mp[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    void def(int ind,string& digits,string path,vector<string>& res){
         if(ind==digits.size()){
             res.push_back(path);
         return;
         }
         int digit=digits[ind]-'0';
-        string letter=mapping[digit];
-        for(int i=0;i<letter.size();i++){
-            path.push_back(letter[i]);
-            dfs(ind+1,digits,path,res);
+        string letters=mp[digit];
+        for(int i=0;i<letters.size();i++){
+            path.push_back(letters[i]);
+            def(ind+1,digits,path,res);
             path.pop_back();
         }
-        
     }
     vector<string> letterCombinations(string digits) {
         vector<string> res;
         string path="";
-        if(digits.size()==0) return res;
-        dfs(0,digits,path,res);
+        def(0,digits,path,res);
         return res;
     }
 };
