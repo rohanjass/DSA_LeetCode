245cc34b5bbeecd5847a5064032cb2e7429462a8
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-12-05 12:24:14 +0530

    Time: 11 ms (68.08%) | Memory: 20.8 MB (62.73%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
+    vector<pair<int,int>> directions={{1,0},{0,1},{-1,0},{0,-1}};
     int def(int i,int j,int preVal,vector<vector<int>>& matrix,vector<vector<int>>& dp){
         if(i<0 || j<0 || i>=matrix.size() || j>=matrix[0].size() || matrix[i][j]<=preVal) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
         int res=1;
-        vector<pair<int,int>> dir={{1,0},{0,1},{-1,0},{0,-1}};
-        for(auto& d:dir){
+        for(auto d:directions){
             int nr=i+d.first;
             int nc=j+d.second;
             res=max(res,1+def(nr,nc,matrix[i][j],matrix,dp));
