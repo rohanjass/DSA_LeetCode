f51ed21141525b199d579c4b33365021e235373d
rohanjass <155191738+rohanjass@users.noreply.github.com>
2025-12-16 19:23:56 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (62.49%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n=edges.size();
-        vector<int> par(n+1),rank(n+1,1);
-        for(int i=0;i<=n;i++){
-            par[i]=i;
+    int findPar(int n,vector<int>& par){
+        if(par[n]!=n){
+            par[n]=findPar(par[n],par);
         }
-
-        for(const auto& edge:edges){
-            if(!unionFind(par,rank,edge[0],edge[1])) return {edge[0],edge[1]};
-        }
-    return {};
+    return par[n];
     }
 
-    int findPar(vector<int>& par,int n){
-        int p=par[n];
-        while(p!=par[p]){
-            par[p]=par[par[p]];
-            p=par[p];
-        }
-    return p;
-    }
-
-    bool unionFind(vector<int>& par,vector<int>& rank,int n1,int n2){
-        int p1=findPar(par,n1);
-        int p2=findPar(par,n2);
+    bool unionFind(int n1,int n2,vector<int>& par,vector<int>& rank){
+        int p1=findPar(n1,par);
+        int p2=findPar(n2,par);
 
         if(p1==p2){
             return false;
-        } 
+        }
         if(rank[p1]>=rank[p2]){
             par[p2]=p1;
             rank[p1]+=rank[p2];
-        } else {
+        } else{
             par[p1]=p2;
             rank[p2]+=rank[p1];
         }
     return true;
     }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n=edges.size();
 
+        vector<int> par(n+1),rank(n+1,1);
+        for(int i=0;i<n;i++){
+            par[i]=i;
+        }
+
+        for(auto const& e:edges){
+            if(!unionFind(e[0],e[1],par,rank)) return {e[0],e[1]};
+        }
+    return {};
+    }
 };
