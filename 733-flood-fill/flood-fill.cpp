0df152686fd3731351a57ed008dcbdedd9f5f248
rohanjass <155191738+rohanjass@users.noreply.github.com>
2026-01-06 01:17:24 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (73.63%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<pair<int,int>> dir={{1,0},{0,1},{-1,0},{0,-1}};
-    void dfs(int r,int c,int tar,vector<vector<int>>& image,int color){
-        if(r<0 || c<0 || r>=image.size() || c>=image[0].size() || image[r][c]!=tar) return;
-        image[r][c]=color;
-        for(const auto& d:dir){
-            int nr=r+d.first;
-            int nc=c+d.second;
-            dfs(nr,nc,tar,image,color);
+    void def(int i,int j,int tar,vector<vector<int>>& image,int color){
+        if(i<0 || j<0 || i>=image.size() || j>=image[0].size() || image[i][j]!=tar) return;
+        image[i][j]=color;
+        for(auto& d:dir){
+            int nr=i+d.first;
+            int nc=j+d.second;
+            def(nr,nc,tar,image,color);
         }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         if(image[sr][sc]==color) return image;
-        dfs(sr,sc,image[sr][sc],image,color);
+        def(sr,sc,image[sr][sc],image,color);
         return image;
     }
 };
