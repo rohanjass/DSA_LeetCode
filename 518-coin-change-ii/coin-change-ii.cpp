2c3fb3d49116c85b77ddbefc7b35d4843673cce4
rohanjass <155191738+rohanjass@users.noreply.github.com>
2026-01-07 16:04:22 +0530

    Time: 47 ms (23.15%) | Memory: 37.1 MB (25.44%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    int dfs(int i,int amount, vector<int>& coins,vector<vector<int>>& dp){
+    int def(int i,int amount,vector<int>& coins,vector<vector<int>>& dp){
         if(amount==0) return 1;
-        if(amount<0) return 0;
-        if(dp[i][amount]!=-1) return dp[i][amount];
         if(i>=coins.size()) return 0;
-        int notTake=dfs(i,amount-coins[i],coins,dp);
-        int take=dfs(i+1,amount,coins,dp);
-    return dp[i][amount]=take+notTake;
+        if(dp[i][amount]!=-1) return dp[i][amount];
+        int t=0;
+        if(coins[i]<=amount){
+            t=def(i,amount-coins[i],coins,dp);
+        }
+        int nt=def(i+1,amount,coins,dp);
+        return dp[i][amount]=t+nt;
     }
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,-1));
-        return dfs(0,amount,coins,dp);
+        vector<vector<int>> dp(coins.size()+1,vector<int>(amount+1,-1));
+        return def(0,amount,coins,dp);
     }
 };
