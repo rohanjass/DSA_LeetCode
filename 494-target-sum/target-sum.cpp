2142046ee4296b0c966f13979d743074a1df7401
rohanjass <155191738+rohanjass@users.noreply.github.com>
2026-01-13 17:18:21 +0530

    Time: 116 ms (39.29%) | Memory: 50.1 MB (15.52%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -5,7 +5,7 @@ public:
         vector<unordered_map<int,int>> dp(n+1);
         dp[0][0]=1;
         for(int i=0;i<n;i++){
-            for(auto& p:dp[i]){
+            for(auto p:dp[i]){
                 int sum=p.first,ways=p.second;
                 dp[i+1][sum+nums[i]]+=ways;
                 dp[i+1][sum-nums[i]]+=ways;
