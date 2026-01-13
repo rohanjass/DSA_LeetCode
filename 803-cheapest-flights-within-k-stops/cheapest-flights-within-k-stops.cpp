a33324c347c62ca2f42b762ac085f8b5ed622ea9
rohanjass <155191738+rohanjass@users.noreply.github.com>
2026-01-13 19:11:05 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (63.28%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int,int>>> adj(n);
+        vector<vector<pair<int,int>>>adj(n);
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>> > minH;
         vector<vector<int>> dist(n,vector<int>(k+5,1e6));
-        priority_queue<pair<int,pair<int,int>>,
-        vector<pair<int,pair<int,int>>>,
-        greater<pair<int,pair<int,int>>> > minH;
+
         for(auto& f:flights){
             adj[f[0]].push_back({f[1],f[2]});
-        } 
+        }
+
         minH.push({0,{src,-1}});
         dist[src][0]=0;
+
         while(!minH.empty()){
             auto node=minH.top();
             minH.pop();
@@ -18,16 +19,17 @@ public:
             int flight=node.second.first;
             int stops=node.second.second;
             if(flight==dst) return cost;
-            if(stops==k || dist[flight][stops+1]<cost) continue;
+            if(stops==k || dist[flight][stops+1]>cost) continue;
 
-            for(auto& [nei,nextCost]:adj[flight]){
-                int nextStop=stops+1;
-                if(dist[nei][nextStop+1]>cost+nextCost){
-                    dist[nei][nextStop+1]=cost+nextCost;
-                    minH.push({cost+nextCost,{nei,nextStop}});
+            for(auto& [newNode,newCost]:adj[flight]){
+                int newStops=stops+1;
+                if(dist[newNode][newStops+1]>newCost+cost){
+                    dist[newNode][newStops+1]=newCost+cost;
+                    minH.push({dist[newNode][newStops+1],{newNode,newStops}});
                 }
             }
         }
+
     return -1;
     }
 };
