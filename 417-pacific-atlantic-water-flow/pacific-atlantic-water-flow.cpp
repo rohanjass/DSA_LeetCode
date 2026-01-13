c94fde39d6a077b2f9e952177c56ce3500c3d75c
rohanjass <155191738+rohanjass@users.noreply.github.com>
2026-01-13 21:59:20 +0530

    Time: 0 ms (100.00%) | Memory: 22.2 MB (79.19%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
     vector<pair<int,int>> dir={{1,0},{0,1},{-1,0},{0,-1}};
-    void dfs(vector<vector<bool>>& oc,int r,int c,vector<vector<int>>& a){
+    void def(vector<vector<bool>>& oc,int r,int c,vector<vector<int>>& a){
         oc[r][c]=true;
-        for(const auto& d:dir){
+        for(auto& d:dir){
             int nr=r+d.first;
             int nc=c+d.second;
-            if(nr>=0 && nc>=0 && nr<a.size() && nc<a[0].size() && oc[nr][nc]==false && a[r][c]<=a[nr][nc]){
-                dfs(oc,nr,nc,a);
-            }
+            if(nr<0 || nc<0 || nr>=a.size() || nc>=a[0].size() || oc[nr][nc]==true || a[nr][nc]<a[r][c]) continue;
+
+            def(oc,nr,nc,a);
         }
-    
     }
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        int m=heights.size(),n=heights[0].size();
-        vector<vector<bool>>pcf(m,vector<bool>(n,false));
-        vector<vector<bool>>atl(m,vector<bool>(n,false));
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& height) {
+        int m=height.size();
+        int n=height[0].size();
+
+        vector<vector<bool>> pcf(m,vector<bool>(n,false));
+        vector<vector<bool>> atl(m,vector<bool>(n,false));
+
         for(int i=0;i<m;i++){
-            dfs(pcf,i,0,heights);
-            dfs(atl,i,n-1,heights);
+            def(pcf,i,0,height);
+            def(atl,i,n-1,height);
         }
+
         for(int i=0;i<n;i++){
-            dfs(pcf,0,i,heights);
-            dfs(atl,m-1,i,heights);
+            def(pcf,0,i,height);
+            def(atl,m-1,i,height);
         }
 
         vector<vector<int>> res;
+
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(pcf[i][j] && atl[i][j]) res.push_back({i,j});
