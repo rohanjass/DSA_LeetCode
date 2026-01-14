82095c6708ac9fb6375d25527e0803372992a627
rohanjass <155191738+rohanjass@users.noreply.github.com>
2026-01-14 14:19:36 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (32.31%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int def(int i,int buy,vector<int>& prices,vector<vector<int>>& dp){
+    int def(int i,bool buy,vector<int>& prices,vector<vector<int>>& dp){
         if(i>=prices.size()) return 0;
         if(dp[i][buy]!=-1) return dp[i][buy];
-        
+
         int coolDown=def(i+1,buy,prices,dp);
+
         if(buy){
-            int buyProfit=def(i+1,0,prices,dp)-prices[i];
-            dp[i][buy]=max(coolDown,buyProfit);
+            int brought=def(i+1,!buy,prices,dp)-prices[i];
+            return dp[i][buy]=max(coolDown,brought);
         } else {
-            int sell=def(i+2,1,prices,dp)+prices[i];
-            dp[i][buy]=max(coolDown,sell);
+            int sell=def(i+2,true,prices,dp)+prices[i];
+            return dp[i][buy]=max(coolDown,sell);
         }
-    return dp[i][buy];
     }
     int maxProfit(vector<int>& prices) {
         vector<vector<int>> dp(prices.size(),vector<int>(2,-1));
-        return def(0,1,prices,dp);
+        return def(0,true,prices,dp);
     }
 };
