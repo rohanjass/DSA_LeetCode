fe80fc505714de1f49ce5c6f061611c4da0008d3
rohanjass <155191738+rohanjass@users.noreply.github.com>
2026-01-21 14:45:09 +0530

    Time: 3 ms (71.33%) | Memory: 13.1 MB (78.75%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    void def(int open,int close,string& path,vector<string>& res){
+    void dfs(int open,int close,string& path,vector<string>& res){
         if(open==close && open==0){
             res.push_back(path);
-        return;
+            return;
         }
-
         if(open>0){
             path.push_back('(');
-            def(open-1,close,path,res);
+            dfs(open-1,close,path,res);
             path.pop_back();
         }
         if(open<close){
             path.push_back(')');
-            def(open,close-1,path,res);
+            dfs(open,close-1,path,res);
             path.pop_back();
         }
     }
     vector<string> generateParenthesis(int n) {
         vector<string> res;
-        string path;
-        def(n,n,path,res);
-
+        string path="";
+        dfs(n,n,path,res);
     return res;
     }
 };
