154b8e67b514cc7bd322d8474fe3d1a479fc28f1
rohanjass <155191738+rohanjass@users.noreply.github.com>
2026-02-11 20:24:33 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (71.02%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    int sumTarget;
+    int sumTar;
     bool def(int i,int k,int target,vector<int>& nums,vector<bool>& used){
         if(k==0) return true;
-        if(target==0) return def(nums.size()-1,k-1,sumTarget,nums,used);
+        if(target==0) return def(nums.size()-1,k-1,sumTar,nums,used);
 
         for(int j=i;j>=0;j--){
-            if(used[j] || target-nums[j]<0){
-                continue;
-            }
+            if(used[j] || target-nums[j]<0) continue;
 
             used[j]=true;
-
             if(def(j-1,k,target-nums[j],nums,used)) return true;
-
             used[j]=false;
-            if(target==sumTarget) break;
+
+            if(target==sumTar) break;
         }
     return false;
     }
     bool canPartitionKSubsets(vector<int>& nums, int k) {
         int n=nums.size();
+        vector<bool> used(n,false);
         int sum=0;
         for(int n:nums){
             sum+=n;
         }
         if(sum%k!=0) return false;
-        sumTarget=sum/k;
-        vector<bool> used(nums.size(),false);
-        return def(nums.size()-1,k,sumTarget,nums,used);
+        sumTar=sum/k;
+
+    return def(n-1,k,sumTar,nums,used);
     }
 };
