3d55c6592a0327893a48f141ea8e2ff71ea8c81f
rohanjass <155191738+rohanjass@users.noreply.github.com>
2026-03-19 14:24:00 +0530

    Time: 1 ms (37.37%) | Memory: 16.6 MB (86.71%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -15,21 +15,22 @@ public:
         while(fresh && !q.empty()){
             int len=q.size();
             for(int i=0;i<len;i++){
-                auto node=q.front();
-                q.pop();
-                int row=node.first;
-                int col=node.second;
+                auto cur=q.front();
+            q.pop();
+            int r=cur.first;
+            int c=cur.second;
 
-                for(auto d:dir){
-                    int nr=row+d.first;
-                    int nc=col+d.second;
+            for(auto d:dir){
+                int nr=r+d.first;
+                int nc=c+d.second;
 
-                    if(nr<0 || nc<0 || nr>=m || nc>=n || grid[nr][nc]!=1) continue;
-                    fresh--;
-                    grid[nr][nc]=2;
-                    q.push({nr,nc});
-                }
+                if(nr<0 || nc<0 || nr==grid.size() || nc==grid[0].size() || grid[nr][nc]!=1) continue;
+
+                fresh--;
+                grid[nr][nc]=2;
+                q.push({nr,nc});
             }
+            } 
         time++;
         }
     return (fresh==0)? time:-1;
