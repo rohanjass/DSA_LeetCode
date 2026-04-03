9e242488467454cfd4a25d25045c700a85a418d8
rohanjass <155191738+rohanjass@users.noreply.github.com>
2026-04-03 20:40:04 +0530

    Time: 7 ms (61.64%) | Memory: 22.3 MB (79.11%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
     vector<pair<int,int>> dir={{1,0},{0,1},{-1,0},{0,-1}};
-    void def(vector<vector<bool>>& oc,int r,int c,vector<vector<int>>& a){
+    void func(int r,int c,vector<vector<bool>>& oc,vector<vector<int>>& a){
         oc[r][c]=true;
         for(auto& d:dir){
             int nr=r+d.first;
             int nc=c+d.second;
-            if(nr<0 || nc<0 || nr>=a.size() || nc>=a[0].size() || oc[nr][nc]==true || a[nr][nc]<a[r][c]) continue;
 
-            def(oc,nr,nc,a);
+            if(nr<0 || nc<0 || nr==a.size() || nc==a[0].size() || oc[nr][nc]==true || a[nr][nc]<a[r][c]) continue;
+
+            func(nr,nc,oc,a);
         }
     }
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& height) {
-        int m=height.size();
-        int n=height[0].size();
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        int m=heights.size();
+        int n=heights[0].size();
 
         vector<vector<bool>> pcf(m,vector<bool>(n,false));
         vector<vector<bool>> atl(m,vector<bool>(n,false));
 
         for(int i=0;i<m;i++){
-            def(pcf,i,0,height);
-            def(atl,i,n-1,height);
+            func(i,0,pcf,heights);
+            func(i,n-1,atl,heights);
         }
-
         for(int i=0;i<n;i++){
-            def(pcf,0,i,height);
-            def(atl,m-1,i,height);
+            func(0,i,pcf,heights);
+            func(m-1,i,atl,heights);
         }
 
         vector<vector<int>> res;
-
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(pcf[i][j] && atl[i][j]) res.push_back({i,j});
