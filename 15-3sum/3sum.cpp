7aa738ddc6ac711c5b22c8c0c8b881eae57b0d27
rohanjass <155191738+rohanjass@users.noreply.github.com>
2026-04-06 21:46:50 +0530

    Time: 54 ms (30.86%) | Memory: 29.2 MB (45.44%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -6,6 +6,7 @@ public:
         vector<vector<int>> res;
         for(int i=0;i<n;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
+
             int l=i+1,r=n-1;
             while(l<r){
                 int sum=nums[i]+nums[l]+nums[r];
@@ -14,11 +15,15 @@ public:
                     l++;
                     r--;
 
-                    while(l<r && nums[l]==nums[l-1])l++;
-                    while(l<r && nums[r]==nums[r+1])r--;
+                    while(l<r && nums[l]==nums[l-1]) l++;
+                    while(l<r && nums[r]==nums[r+1]) r--;
                 }
-                else if(sum<0) l++;
-                else r--;
+                else if(sum<0){
+                    l++;
+                } 
+                else{
+                    r--;
+                }
             }
         }
     return res;
