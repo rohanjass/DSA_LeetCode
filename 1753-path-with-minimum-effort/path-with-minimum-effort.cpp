f1592a143116b5794de992f5a6c13c7b42db2834
rohanjass <155191738+rohanjass@users.noreply.github.com>
2026-04-22 14:36:02 +0530

    Time: 315 ms (5.04%) | Memory: 67.2 MB (7.06%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -2,28 +2,27 @@ class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         int m=heights.size(),n=heights[0].size();
-        vector<pair<int,int>> dir={{1,0},{0,1},{-1,0},{0,-1}};
         priority_queue<vector<int>,vector<vector<int>>,greater<>> minH;
+        vector<pair<int,int>> dir={{1,0},{0,1},{-1,0},{0,-1}};
         set<pair<int,int>> visited;
         minH.push({0,0,0});
 
         while(!minH.empty()){
-            auto curr=minH.top();
+            auto cur=minH.top();
             minH.pop();
-            int node=curr[0];
-            int r=curr[1];
-            int c=curr[2];
-
-            if(r==m-1 && c==n-1) return node;
+            int curDiff=cur[0];
+            int r=cur[1];
+            int c=cur[2];
+            if(r==m-1 && c==n-1) return curDiff;
             if(visited.count({r,c})) continue;
             visited.insert({r,c});
+            
             for(auto& d:dir){
                 int nr=r+d.first;
                 int nc=c+d.second;
-
-                if(nr<0 || nc<0 || nr>=m || nc>=n || visited.count({nr,nc})) continue;
+                if(nr<0 || nc<0 || nr==m || nc==n || visited.count({nr,nc})) continue;
                 int diff=abs(heights[r][c]-heights[nr][nc]);
-                minH.push({max(node,diff),nr,nc});
+                minH.push({max(curDiff,diff),nr,nc});
             }
         }
     return -1;
