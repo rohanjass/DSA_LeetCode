25a755e0896eafb059977880373485733a9894d8
rohanjass <155191738+rohanjass@users.noreply.github.com>
2026-05-04 18:57:55 +0530

    Time: 76 ms (6.41%) | Memory: 34.4 MB (8.56%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -3,9 +3,15 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>> mp;
         for(const auto& s:strs){
-            string sortedS=s;
-            sort(sortedS.begin(),sortedS.end());
-        mp[sortedS].push_back(s);
+            vector<int>freq (26,0);
+            for(char c:s){
+                freq[c-'a']++;
+            }
+            string key="";
+            for(int i=0;i<26;i++){
+                key+=to_string(freq[i])+",";
+            }
+        mp[key].push_back(s);
         }
         vector<vector<string>> res;
         for(auto& p:mp){
