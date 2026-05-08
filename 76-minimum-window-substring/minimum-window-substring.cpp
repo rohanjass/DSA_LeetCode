25067feb5846f42e92a24856c4671085ae4cb2fe
rohanjass <155191738+rohanjass@users.noreply.github.com>
2026-05-08 12:35:52 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (61.79%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -2,27 +2,28 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         vector<int> mp(128,0);
-        int cnt=0,l=0,r=0,startInd=-1;
-        int minLen=INT_MAX;
-        for(char c:t){
-            mp[c]++;
+        int n=s.size();
+        for(char i:t){
+            mp[i]++;
         }
 
-        while(r<s.size()){
-            if(mp[s[r]]>0) cnt++;
+        int l=0,minWin=INT_MAX,minInd=-1,count=0;
+
+        for(int r=0;r<n;r++){
+            if(mp[s[r]]>0) count++;
             mp[s[r]]--;
 
-            while(cnt==t.size()){
-                if(r-l+1<minLen){
-                    minLen=r-l+1;
-                    startInd=l;
+            while(count==t.size()){
+                if((r-l+1)<minWin){
+                    minWin=r-l+1;
+                    minInd=l;
                 }
+
                 mp[s[l]]++;
-                if(mp[s[l]]>0) cnt--;
+                if(mp[s[l]]>0) count--;
                 l++;
             }
-        r++;
         }
-    return (startInd==-1)? "":s.substr(startInd,minLen);
+    return (minInd==-1)? "":s.substr(minInd,minWin);
     }
 };
