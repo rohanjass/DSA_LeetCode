3f1ad51002f34fe734ce036c9b1334bdf242598f
rohanjass <155191738+rohanjass@users.noreply.github.com>
2026-05-10 14:10:41 +0530

    Time: 207 ms (21.92%) | Memory: 13.9 MB (25.72%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int dfs(vector<int>& arr,int l,int r,vector<vector<int>>& dp){
+    int func(int l,int r,vector<int>& nums,vector<vector<int>>& dp){
         if(l>r) return 0;
         if(dp[l][r]!=-1) return dp[l][r];
-
+        int res=0;
         for(int i=l;i<=r;i++){
-            int coins=arr[l-1]*arr[i]*arr[r+1];
-            coins+=dfs(arr,l,i-1,dp)+dfs(arr,i+1,r,dp);
-            dp[l][r]=max(coins,dp[l][r]);
+            int cost=nums[l-1]*nums[i]*nums[r+1];
+            res=max(res,cost+func(l,i-1,nums,dp)+func(i+1,r,nums,dp));
         }
-        return dp[l][r];
+    return dp[l][r]=res;
     }
     int maxCoins(vector<int>& nums) {
         int n=nums.size();
         vector<int> newNums(n+2,1);
         vector<vector<int>> dp(n+2,vector<int>(n+2,-1));
+
         for(int i=0;i<n;i++){
             newNums[i+1]=nums[i];
         }
-    return dfs(newNums,1,newNums.size()-2,dp);
+    return func(1,newNums.size()-2,newNums,dp);
     }
 };
