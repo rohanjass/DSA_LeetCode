3b2a0793e85f600340b3f1697a24e7200950f66f
rohanjass <155191738+rohanjass@users.noreply.github.com>
2026-05-29 12:54:27 +0530

    Time: 13 ms (12.43%) | Memory: 19.2 MB (20.25%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -2,34 +2,30 @@ class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         vector<vector<pair<int,int>>> adj(n);
-        priority_queue<vector<int>,vector<vector<int>>,greater<>> minH;
-        vector<vector<int>> dist(n,vector<int>(k+2,1e9));
-
         for(auto& f:flights){
             adj[f[0]].push_back({f[1],f[2]});
         }
-
-        minH.push({0,src,-1});
+        priority_queue<vector<int>,vector<vector<int>>,greater<>> minh;
+        vector<vector<int>> dist(n,vector<int>(k+2,INT_MAX));
+        //minh [ cost , node , stops ]
+        //dist [ node , stops ]
+        minh.push({0,src,-1}); 
         dist[src][0]=0;
-
-        while(!minH.empty()){
-            auto cur=minH.top();
-            minH.pop();
-
+        while(!minh.empty()){
+            auto cur=minh.top();
+            minh.pop();
             int cost=cur[0];
             int node=cur[1];
             int stops=cur[2];
-
             if(node==dst) return cost;
 
-            if(stops==k || cost>dist[node][stops+1]) continue;
+            if(stops==k || dist[node][stops+1]<cost) continue;
 
             for(auto& [nNode,nCost]:adj[node]){
                 int nextStop=stops+1;
-
                 if(dist[nNode][nextStop+1]>nCost+cost){
                     dist[nNode][nextStop+1]=nCost+cost;
-                    minH.push({nCost+cost,nNode,nextStop});
+                    minh.push({(nCost+cost),nNode,nextStop});
                 }
             }
         }
