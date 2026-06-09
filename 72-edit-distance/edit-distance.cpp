f9ee7c5757fc48c31d00826daa4ebd4afccd4cec
rohanjass <155191738+rohanjass@users.noreply.github.com>
2026-06-09 13:58:52 +0530

    Time: 6 ms (70.20%) | Memory: 14.3 MB (16.40%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int def(int i,int j,string& word1, string& word2,vector<vector<int>>& dp,int m,int n){
-        if(i==m) return n-j;//insert left part into s1
-        if(j==n) return m-i;//deleting part left in st1
-
+    int m,n;
+    int func(int i,int j,string& word1,string& word2,vector<vector<int>>& dp){
+        if(i==m) return n-j;
+        if(j==n) return m-i;
         if(dp[i][j]!=-1) return dp[i][j];
         if(word1[i]==word2[j]){
-            return dp[i][j]=def(i+1,j+1,word1,word2,dp,m,n);
+            return dp[i][j]=func(i+1,j+1,word1,word2,dp);
         }
-        int op1=def(i,j+1,word1,word2,dp,m,n);
-        int op2=def(i+1,j,word1,word2,dp,m,n);
-        int op3=def(i+1,j+1,word1,word2,dp,m,n);
+        int op1=func(i+1,j,word1,word2,dp);
+        int op2=func(i+1,j+1,word1,word2,dp);
+        int op3=func(i,j+1,word1,word2,dp);
     return dp[i][j]=1+min({op1,op2,op3});
     }
     int minDistance(string word1, string word2) {
-        int m=word1.size();
-        int n=word2.size();
-        vector<vector<int>> dp (m,vector<int>(n,-1));
-        return def(0,0,word1,word2,dp,m,n);
+        m=word1.size(),n=word2.size();
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+    return func(0,0,word1,word2,dp);
     }
 };
