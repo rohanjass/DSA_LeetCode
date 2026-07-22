7d12f8bd28dd1646c1aab2be0216230ca237e036
rohanjass <155191738+rohanjass@users.noreply.github.com>
2026-07-22 12:08:27 +0530

    Time: 3 ms (67.97%) | Memory: 10.9 MB (71.43%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int n=s.size();
         vector<int> freq(26,0);
-        int l=0,maxFreq=0,res=0;
+        int n=s.size();
+        int l=0,res=0,maxFreq=0;
+        
         for(int r=0;r<n;r++){
             freq[s[r]-'A']++;
             maxFreq=max(maxFreq,freq[s[r]-'A']);
@@ -12,7 +13,7 @@ public:
                 freq[s[l]-'A']--;
                 l++;
             }
-        res=max(res,r-l+1);
+        res=max(res,(r-l+1));
         }
     return res;
     }
