75e115866551efde9dd81e83e312a948b021e124
rohanjass <155191738+rohanjass@users.noreply.github.com>
2026-07-22 12:47:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (70.81%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        if(s1.size()>s2.size()) return false;
-        vector<int> countS1(26,0),countWindow(26,0);
-        for(char c:s1) countS1[c-'a']++;
-        for(int i=0;i<s2.size();i++){
-            countWindow[s2[i]-'a']++;
-            
-            if(i>=s1.size()){
-                countWindow[s2[i-s1.size()]-'a']--;
-            }
+        int n1=s1.size();
+        int n2=s2.size();
+        vector<int> count1(26,0);
+        vector<int> count2(26,0);
 
-            if(countS1==countWindow) return true;
+        if(n1>n2) return false;
+
+        for(int i=0;i<n1;i++){
+            count1[s1[i]-'a']++;
+        }
+
+        for(int i=0;i<n2;i++){
+            count2[s2[i]-'a']++;
+
+            if(i>=n1) count2[s2[i-n1]-'a']--;
+
+            if(count1==count2) return true;
         }
     return false;
     }
