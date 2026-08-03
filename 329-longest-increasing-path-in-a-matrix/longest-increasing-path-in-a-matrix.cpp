a161b701a6c4b156bd7ceb38f3abe3a903f6caf2
rohanjass <155191738+rohanjass@users.noreply.github.com>
2026-08-03 12:10:26 +0530

    Time: 4 ms (91.71%) | Memory: 20.7 MB (86.30%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     vector<pair<int,int>> dir={{1,0},{0,1},{-1,0},{0,-1}};
-    int def(int i,int j,int preVal,vector<vector<int>>& matrix,vector<vector<int>>& dp){
-        if(i<0 || j<0 || i>=matrix.size() || j>=matrix[0].size() || matrix[i][j]<=preVal) return 0;
+    int m,n;
+    int findLongestPath(int i,int j,int pre,vector<vector<int>>& matrix,vector<vector<int>>& dp){
+        if(i<0 || j<0 || i==m || j==n || matrix[i][j]<=pre) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        int res=1;
-        for(auto d:dir){
-            int nr=i+d.first;
-            int nc=j+d.second;
-            res=max(res,1+def(nr,nc,matrix[i][j],matrix,dp));
+        int res=0;
+        for(auto& d:dir){
+            int r=i+d.first;
+            int c=j+d.second;
+            res=max(res,1+findLongestPath(r,c,matrix[i][j],matrix,dp));
         }
     return dp[i][j]=res;
     }
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-        int res=-1;
-        vector<vector<int>> dp(m,vector<int>(n,-1));
+        m=matrix.size();
+        n=matrix[0].size();
+        vector<vector<int>>dp (m,vector<int>(n,-1));
+        int res=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                res=max(res,def(i,j,INT_MIN,matrix,dp));
+                res=max(res,findLongestPath(i,j,INT_MIN,matrix,dp));
             }
         }
     return res;
